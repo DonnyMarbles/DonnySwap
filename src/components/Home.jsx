074e32d926d6a52a8de1f690da4f30a0d1b8f9b0
jychoos <darianremp@gmail.com>
2024-08-20 14:46:06 -0400

    DonnySwap v0.1.02 - Updated Home.jsx with MRBL contract

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { HomeContainer, TokenInfo } from '../styles/HomeStyles';
+
+
+
 const Home = () => (
+  
   <HomeContainer>
     <div>
-    <h1>Welcome to DonnySwap v0.1.0</h1>
+    <h1>Welcome to DonnySwap!<br/>👑v0.1.02</h1>
+    <a href="https://x.com/Donny_Marbles"><img src="./src/assets/MRBL_logo.png" alt="Token Logo" width="60" /></a>
     <h1>Current features</h1>
+    
     <ol>
       <li><h2>🔒Create LPs</h2></li>
       Create ERC-20 - ERC-20 LPs <br/>
@@ -22,10 +28,14 @@ const Home = () => (
       <li><h2>✅Strict Token List</h2></li>
       Current iteration only supports MRBL, KRST, WKREST <br/>
       To have your ERC-20 added, send an <a href="mailto:better.future.labs@gmail.com">email</a><br/>
-      <li><h2>📖Open-sourced</h2></li>
-      Wrapped KREST Contract: <a href="https://krest.subscan.io/account/0xDd11f4E48CE3A2B9043B2B0758ce704d3Fd191dc?tab=contract">Subscan</a> <br/>
+      <li><h2>📖Open-source Contracts</h2></li>
+      <h3>Tokens:</h3>
+      <img src="./src/assets/MRBL_logo.png" alt="Token Logo" width="20" />Marbles (MRBL) Contract: <a href="https://krest.subscan.io/account/0xf5321b84aae2990FEC72FD57aF1B4a99B01EB928">Subscan</a> <br/>
+      <img src="./src/assets/WKREST_logo.png" alt="Token Logo" width="20" />Wrapped KREST (WKREST) Contract: <a href="https://krest.subscan.io/account/0xDd11f4E48CE3A2B9043B2B0758ce704d3Fd191dc?tab=contract">Subscan</a> <br/>
+      <h3>Backend Contracts:</h3>
       UniswapV2Router02 Contract: <a href="https://krest.subscan.io/account/0xf441807ed1943925f6f887660c44d7eb2ecc17c2?tab=contract">Subscan</a> <br/>
       SushiSwapFactory Contract: <a href="https://krest.subscan.io/account/0x23aAC8C182b2C2a2387868ee98C1544bF705c097?tab=contract">Subscan</a> <br/>
+      
       Front-end Github Repo: <a href="https://github.com/DonnyMarbles/DonnySwap">Github</a><br/><br/>
 
       Recommended H160 (EVM) Wallet: 🦊 <a href="https://metamask.io/">Metamask</a> <br/>
