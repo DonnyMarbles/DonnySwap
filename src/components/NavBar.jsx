689822346b119e62f464ee0a32740ee29eb0beb2
jychoos <darianremp@gmail.com>
2024-08-22 15:35:01 -0400

    DonnySwap v0.1.251 - Made NavBar more responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,31 +8,33 @@ const NavBar = () => {
   const [dropdownVisibleBalances, setDropdownVisibleBalances] = useState(false);
 
   const toggleDropdownLiquidity = () => {
-    setDropdownVisibleLiquidity(!dropdownVisibleLiquidity);
+    setDropdownVisibleLiquidity((prev) => !prev);
+    setDropdownVisibleBalances(false); // Close other dropdown
   };
+
   const toggleDropdownBalances = () => {
-    setDropdownVisibleBalances(!dropdownVisibleBalances);
+    setDropdownVisibleBalances((prev) => !prev);
+    setDropdownVisibleLiquidity(false); // Close other dropdown
   };
-  const closeDropdownLiquidity = () => {
+
+  const closeDropdowns = () => {
     setDropdownVisibleLiquidity(false);
-  };
-  const closeDropdownBalances = () => {
     setDropdownVisibleBalances(false);
   };
 
   return (
     <NavBarContainer>
       <NavLinks>
-        <Link to="/">Home</Link>
-        <Link to="/swap">Swap</Link>
+        <Link to="/" onClick={closeDropdowns}>Home</Link>
+        <Link to="/swap" onClick={closeDropdowns}>Swap</Link>
         <DropdownContainer>
           <DropdownButton onClick={toggleDropdownLiquidity}>
             Liquidity
           </DropdownButton>
           {dropdownVisibleLiquidity && (
             <DropdownContent>
-              <Link to="/add-liquidity" onClick={closeDropdownLiquidity}>Add Liquidity</Link>
-              <Link to="/remove-liquidity" onClick={closeDropdownLiquidity}>Remove Liquidity</Link>
+              <Link to="/add-liquidity" onClick={closeDropdowns}>Add Liquidity</Link>
+              <Link to="/remove-liquidity" onClick={closeDropdowns}>Remove Liquidity</Link>
             </DropdownContent>
           )}
         </DropdownContainer>
@@ -42,12 +44,12 @@ const NavBar = () => {
           </DropdownButton>
           {dropdownVisibleBalances && (
             <DropdownContent>
-              <Link to="/token-pairs" onClick={closeDropdownBalances}>LP Token Pairs</Link>
-              <Link to="/token-balances" onClick={closeDropdownBalances}>Token Balances</Link>
+              <Link to="/token-pairs" onClick={closeDropdowns}>LP Token Pairs</Link>
+              <Link to="/token-balances" onClick={closeDropdowns}>Token Balances</Link>
             </DropdownContent>
           )}
         </DropdownContainer>
-        <Link to="/address-converter">Address Converter</Link>
+        <Link to="/address-converter" onClick={closeDropdowns}>Address Converter</Link>
       </NavLinks>
       <CustomConnectButton />
     </NavBarContainer>
